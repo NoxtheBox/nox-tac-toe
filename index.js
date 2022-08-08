afb12561dcc53db870dcc03d2d9a60ceae8291c2
NoxtheBox <87660149+NoxtheBox@users.noreply.github.com>
2022-08-07 23:04:48 -0400

    added metadata

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "public", "home.html"));
 });
 
+app.get("/api/cover-image", async (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "cover.png"));
+});
+
 app.listen(process.env.PORT || 3000, () =>
   console.log("http://localhost:3000")
 );
